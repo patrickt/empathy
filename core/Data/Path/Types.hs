56db7de10adb3c5c20050ca52b5b7394b9123887
Patrick Thomson <patrickt@github.com>
2019-10-29 00:15:23 -0400

    forbid string instances

--- core/Data/Path/Types.hs
+++ core/Data/Path/Types.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE DataKinds, KindSignatures, GADTs #-}
+{-# LANGUAGE DataKinds, GADTs, KindSignatures, RankNTypes, TypeOperators, UndecidableInstances #-}
 
 module Data.Path.Types
     ( Path (..)
@@ -8,6 +7,9 @@ module Data.Path.Types
     , (</>)
     , combine
     ) where
+
+import Data.String
+import GHC.TypeLits
 
 data Relative = Abs | Rel
 
@@ -25,6 +27,11 @@ data Path os (ar :: Relative) (fd :: Entity) where
     Path os ar 'Dir
     -> Path os 'Rel fd
     -> Path os ar fd
+
+instance TypeError ('Text "Path does not provide an IsString instance."
+                    ':$$: 'Text "Use the 'rel' or 'abs' family of functions instead."
+                   ) => IsString (Path os ar fd) where
+  fromString = error "uninhabited"
 
 -- | Join a directory (relative or absolute) with a relative component (file or directory).
 combine :: Path os ar 'Dir -> Path os 'Rel fd -> Path os ar fd
