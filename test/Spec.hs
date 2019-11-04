8f1a20301a8660c5c6394e0e2ae3fcd370496294
Patrick Thomson <patrickt@github.com>
2019-11-04 17:26:35 -0500

    Rework test suite.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,29 @@
 module Main (main) where
 
 import qualified Test.Tasty as Tasty
-import           Test.Tasty.HUnit ((@=?))
+import           Test.Tasty.HUnit ((@?=))
 import qualified Test.Tasty.HUnit as HUnit
+import           Data.Path (Path)
 import qualified Data.Path as Path
+
+assertParsesInto :: String -> Path os ar -> HUnit.Assertion
+assertParsesInto a b = case Path.parse a of
+  Left s  -> HUnit.assertFailure ("error message: " <> s)
+  Right p -> p @?= b
+
+test_introduce :: Tasty.TestTree
+test_introduce = Tasty.testGroup "introduction"
+  [ HUnit.testCase "parse / == rootDir" ("/" `assertParsesInto` Path.rootDir)
+  ]
+
+test_eliminate :: Tasty.TestTree
+test_eliminate = Tasty.testGroup "elimination"
+  [ HUnit.testCase "root == /" (Path.toString Path.rootDir @?= "/")
+  ]
+
 
 main :: IO ()
 main = Tasty.defaultMain $ Tasty.testGroup "empathy"
-  [ HUnit.testCase "root == /" (Path.toString Path.rootDir @=? "/")
+  [ test_introduce
+  , test_eliminate
   ]
