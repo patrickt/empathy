c9a26f3e6130581d90ed517368c7353e14358f53
Patrick Thomson <patrickt@github.com>
2019-11-04 16:44:12 -0500

    Add planfile and some more Haddocks.

--- sig/Data/Path/Generic.hs
+++ sig/Data/Path/Generic.hs
@@ -1,19 +1,26 @@
 {-# LANGUAGE DataKinds, GADTs, LambdaCase, ScopedTypeVariables, TypeApplications, RankNTypes #-}
 
 -- | You probably don't need to import this module directly. If you're working with cross-platform paths, use "Data.Path.Types"; if you're working with your current system path, use "Data.Path".
+--
+-- This module is meant to be imported qualified, e.g.:
+--
+-- @
+--  import Data.Path (Path)
+--  import qualified Data.Path as Path
+-- @
 module Data.Path.Generic
   ( System
   , Path
   , Anchor (..)
   , Entity (..)
+  -- * Constructing
+  , currentDir
+  , rootDir
   -- * Eliminating paths
   , toString
   , chooseAbsRel
   , chooseFileDir
   , choose
-  -- * Predefined constants
-  , currentDir
-  , rootDir
   -- * Building paths
   , combine
   , (</>)
