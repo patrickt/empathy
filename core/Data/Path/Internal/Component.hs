5e52f3b8337bd3c9c105fdd251b0825b74555efe
Patrick Thomson <patrickt@github.com>
2021-06-22 19:42:47 -0400

    hmm looks like order is significant? wtf

--- core/Data/Path/Internal/Component.hs
+++ core/Data/Path/Internal/Component.hs
@@ -1,26 +1,26 @@
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Data.Path.Internal.Component (module Data.Path.Internal.Component) where
 
-import Control.DeepSeq (NFData)
 import Control.Applicative ((<|>))
+import Control.DeepSeq (NFData)
+import Control.Monad (void)
 import Data.ByteString.Builder (Builder)
 import Data.ByteString.Builder qualified as Builder
 import Data.Data (Data)
-import Data.Path.Internal.Raw (RawFilePath)
-import Data.Word8 (Word8, toUpper, _backslash, _slash, _colon)
-import Data.Void (Void)
 import Data.Hashable (Hashable)
+import Data.Path.Internal.Raw (RawFilePath)
+import Data.Void (Void)
+import Data.Word8 (Word8, toUpper, _backslash, _colon, _slash)
 import GHC.Generics (Generic)
-import Text.Megaparsec (Parsec, choice, takeWhile1P, some, try)
-import Text.Megaparsec.Byte (letterChar, char)
-import Control.Monad (void)
+import Text.Megaparsec (Parsec, choice, some, takeWhile1P, try)
+import Text.Megaparsec.Byte (char, letterChar)
 
 data Prefix
   = Verbatim RawFilePath
@@ -51,25 +51,27 @@ renderPrefix = \case
   Disk c -> Builder.word8 c <> ":\\"
 
 parsePrefix :: Parsec Void RawFilePath Prefix
-parsePrefix = choice [
-    -- \\?\UNC\<server>\<share\
-    VerbatimUNC <$> ("\\\\?\\UNC\\" *> verbatimPathComponent <* verbatimPathSep)
-                <*> verbatimPathComponent
-    -- \\?\C:\
-  , VerbatimDisk <$> ("\\\\?\\" *> try drive <* verbatimPathSep)
-    -- \\?\<prefix>\
-  , Verbatim <$> ("\\\\?\\" *> verbatimPathComponent)
-    -- \\.\COM42\
-  , DeviceNS <$> ("\\\\.\\" *> pathComponent)
-    -- \\<server>\<share>\
-  , UNC <$> ("\\\\" *> pathComponent)
-        <*> pathComponent
-    -- C:\
-  , Disk . toUpper <$> drive <* some pathSep ]
+parsePrefix =
+  choice
+    [ -- \\?\<prefix>\
+      try (Verbatim <$> ("\\\\?\\" *> verbatimPathComponent)),
+      -- \\?\UNC\<server>\<share\
+      VerbatimUNC <$> ("\\\\?\\UNC\\" *> verbatimPathComponent <* verbatimPathSep)
+        <*> verbatimPathComponent,
+      -- \\?\C:\
+      VerbatimDisk <$> ("\\\\?\\" *> try drive <* verbatimPathSep),
+      -- \\.\COM42\
+      DeviceNS <$> ("\\\\.\\" *> pathComponent),
+      -- \\<server>\<share>\
+      UNC <$> ("\\\\" *> pathComponent)
+        <*> pathComponent,
+      -- C:\
+      Disk . toUpper <$> drive <* some pathSep
+    ]
   where
     verbatimPathComponent = takeWhile1P Nothing (/= _backslash)
     pathComponent = takeWhile1P Nothing isPathSep <* some pathSep
     isPathSep c = c /= _backslash || c /= _slash
     drive = letterChar <* char _colon
-    pathSep = void (char _backslash  <|> char _slash)
+    pathSep = void (char _backslash <|> char _slash)
     verbatimPathSep = void $ char _backslash
